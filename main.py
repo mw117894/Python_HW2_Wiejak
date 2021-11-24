d6731525bf7382d8d74c96e49dcf76e3bc7ae9c8
MWiejak <mateuszwiejak0@gmail.com>
2021-11-24 17:02:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+def countWords(path):
+    with open(path, "r") as f:
+        text = f.read()
+    f.close()
+
+    wordsCount = {}
+
+    for word in re.findall("\w+",text):
+        word = word.lower()
+        if word in wordsCount.keys():
+            wordsCount[word] += 1
+        else:
+            wordsCount[word] = 1
+
+    print("wordsCount: " + wordsCount.__str__())
+
+countWords("unprocessed_text")
+
+
